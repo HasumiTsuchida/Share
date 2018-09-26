ba62377140f84b0f8af43f00577b3e963c3ce2c3
HasumiTsuchida <1mr2117t@gmail.com>
2018-09-26 10:53:20 +0900

    Button処理をメソッド化してみました

--- app/src/main/java/com/example/admin/share/Setting_Personal.java
+++ app/src/main/java/com/example/admin/share/Setting_Personal.java
@@ -15,6 +15,32 @@ public class Setting_Personal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting__personal);
 
+
+
+        OriginalFragmentPagerAdapter adapter = new OriginalFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        switch (viewPager.getId()){
+            case 1:
+                Button();
+                Intent intent = new Intent(Setting_Personal.this,Setting_Personal.class);
+                startActivity(intent);
+            case 2:
+                Button();
+                Intent intent2 = new Intent(Setting_Personal.this,Setting_Group.class);
+                startActivity(intent2);
+            case 3:
+                Button();
+                Intent intent3 = new Intent(Setting_Personal.this,Setting_Other.class);
+                startActivity(intent3);
+
+        }
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    protected void Button(){
         Button setting_personal_button=(Button)findViewById(R.id.setting_personal_button);
         Button back_login_top_button=(Button)findViewById(R.id.back_login_top_button);
 
@@ -33,25 +59,5 @@ public class Setting_Personal extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        OriginalFragmentPagerAdapter adapter = new OriginalFragmentPagerAdapter(getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        switch (viewPager.getId()){
-            case 1:
-                Intent intent = new Intent(Setting_Personal.this,Setting_Personal.class);
-                startActivity(intent);
-            case 2:
-                Intent intent2 = new Intent(Setting_Personal.this,Setting_Group.class);
-                startActivity(intent2);
-            case 3:
-                Intent intent3 = new Intent(Setting_Personal.this,Setting_Other.class);
-                startActivity(intent3);
-
-        }
-        viewPager.setOffscreenPageLimit(3);
-        viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
